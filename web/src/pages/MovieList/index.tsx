5243c44c3d5893758b28b5831cca5184250c2a1f
masaabuaisheh <masaabuaisheh11@gmail.com>
2024-08-04 19:32:08 +0300

    new feature movie list

--- web/src/pages/MovieList/index.tsx
+++ web/src/pages/MovieList/index.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import ReactPaginate from "react-paginate";
 
 // Define a TypeScript type for the movie object
 type Movie = {
@@ -21,7 +22,7 @@ type Movie = {
   website: string;
 };
 
-//Component State Initialization
+// Component State Initialization
 function MovieListManager() {
   const [listOfMovie, setListOfMovie] = useState<Movie[]>([]);
   const [watchList, setWatchList] = useState<Movie[]>([]);
@@ -66,23 +67,15 @@ function MovieListManager() {
     movie.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-  const handleShowMoreWatchList = () => {
-    setCurrentPageWatchList((prev) => prev + 1);
+  const changePageWatchList = ({ selected }: { selected: number }) => {
+    setCurrentPageWatchList(selected);
   };
 
-  const handleShowLessWatchList = () => {
-    setCurrentPageWatchList((prev) => Math.max(prev - 1, 0));
+  const changePageWatchedList = ({ selected }: { selected: number }) => {
+    setCurrentPageWatchedList(selected);
   };
 
-  const handleShowMoreWatchedList = () => {
-    setCurrentPageWatchedList((prev) => prev + 1);
-  };
-
-  const handleShowLessWatchedList = () => {
-    setCurrentPageWatchedList((prev) => Math.max(prev - 1, 0));
-  };
-
-  //Rendering the Component
+  // Rendering the Component
   return (
     <div className='main-page'>
       <h1 className='title'>Movie List Manager</h1>
@@ -96,80 +89,82 @@ function MovieListManager() {
       <div className='listsContainer'>
         <div className='movieList'>
           <h2>Watch List</h2>
-          {filteredWatchList.slice(0, (currentPageWatchList + 1) * itemsPerPage).map((movie) => (
-            <div key={movie.id}>
-              {/*<p>{movie.id}</p>*/}
-              <h3>{movie.title}</h3>
-              <p><strong>Plot:</strong> {movie.plot}</p>
-              <button className='handleButton' onClick={() => addToWatched(movie)}>Add to Watched</button>
-              <button className='toggleButton' onClick={() => toggleDetails(movie.id)}>
-                {detailsVisible === movie.id ? 'Show Less' : 'Read More Info'}
-              </button>
-              {detailsVisible === movie.id && (
-                <div>
-                  <p><strong>Year:</strong> {movie.year}</p>
-                  <p><strong>Genre:</strong> {movie.genre.join(', ')}</p>
-                  <p><strong>Rating:</strong> {movie.rating}</p>
-                  <p><strong>Director:</strong> {movie.director}</p>
-                  <p><strong>Actors:</strong> {movie.actors.join(', ')}</p>
-                  <p><strong>Poster:</strong> <a href={movie.poster} target="_blank">Watch Poster</a></p>
-                  <p><strong>Trailer:</strong> <a href={movie.trailer} target="_blank">Watch Trailer</a></p>
-                  <p><strong>Run Time:</strong> {movie.runtime} minutes</p>
-                  <p><strong>Awards:</strong> {movie.awards}</p>
-                  <p><strong>Country:</strong> {movie.country}</p>
-                  <p><strong>Language:</strong> {movie.language}</p>
-                  <p><strong>Box Office:</strong> {movie.boxOffice}</p>
-                  <p><strong>Production:</strong> {movie.production}</p>
-                  <p><strong>Website:</strong> <a href={movie.website} target="_blank">Visit Website</a></p>
-                </div>
-              )}
-            </div>
-          ))}
-          {currentPageWatchList > 0 && (
-            <button className='handleButtonList' onClick={handleShowLessWatchList}>Show Less</button>
-          )}
-          {filteredWatchList.length > (currentPageWatchList + 1) * itemsPerPage && (
-            <button className='handleButtonList' onClick={handleShowMoreWatchList}>Show More</button>
-          )}
+          {filteredWatchList.slice(currentPageWatchList * itemsPerPage, (currentPageWatchList + 1) * itemsPerPage)
+            .map((movie) => (
+              <div key={movie.id}>
+                <h3>{movie.title}</h3>
+                <p><strong>Plot:</strong> {movie.plot}</p>
+                <button className='handleButton' onClick={() => addToWatched(movie)}>Add to Watched</button>
+                <button className='toggleButton' onClick={() => toggleDetails(movie.id)}>
+                  {detailsVisible === movie.id ? 'Show Less' : 'Read More Info'}
+                </button>
+                {detailsVisible === movie.id && (
+                  <div>
+                    <p><strong>Year:</strong> {movie.year}</p>
+                    <p><strong>Genre:</strong> {movie.genre.join(', ')}</p>
+                    <p><strong>Rating:</strong> {movie.rating}</p>
+                    <p><strong>Director:</strong> {movie.director}</p>
+                    <p><strong>Actors:</strong> {movie.actors.join(', ')}</p>
+                    <p><strong>Poster:</strong> <a href={movie.poster} target="_blank">Watch Poster</a></p>
+                    <p><strong>Trailer:</strong> <a href={movie.trailer} target="_blank">Watch Trailer</a></p>
+                    <p><strong>Run Time:</strong> {movie.runtime} minutes</p>
+                    <p><strong>Awards:</strong> {movie.awards}</p>
+                    <p><strong>Country:</strong> {movie.country}</p>
+                    <p><strong>Language:</strong> {movie.language}</p>
+                    <p><strong>Box Office:</strong> {movie.boxOffice}</p>
+                    <p><strong>Production:</strong> {movie.production}</p>
+                    <p><strong>Website:</strong> <a href={movie.website} target="_blank">Visit Website</a></p>
+                  </div>
+                )}
+              </div>
+            ))}
+          <ReactPaginate
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={Math.ceil(filteredWatchList.length / itemsPerPage)}
+            onPageChange={changePageWatchList}
+            containerClassName={"pagination"}
+          />
         </div>
 
         <div className='movieList'>
           <h2>Watched List</h2>
-          {filteredWatchedList.slice(0, (currentPageWatchedList + 1) * itemsPerPage).map((movie) => (
-            <div key={movie.id}>
-              {/*<p>{movie.id}</p>*/}
-              <h3>{movie.title}</h3>
-              <p><strong>Plot:</strong> {movie.plot}</p>
-              <button className='handleButton' onClick={() => removeFromWatched(movie)}>Remove from Watched</button>
-              <button className='toggleButton' onClick={() => toggleDetails(movie.id)}>
-                {detailsVisible === movie.id ? 'Show Less' : 'Read More Info'}
-              </button>
-              {detailsVisible === movie.id && (
-                <div>
-                  <p><strong>Year:</strong> {movie.year}</p>
-                  <p><strong>Genre:</strong> {movie.genre.join(', ')}</p>
-                  <p><strong>Rating:</strong> {movie.rating}</p>
-                  <p><strong>Director:</strong> {movie.director}</p>
-                  <p><strong>Actors:</strong> {movie.actors.join(', ')}</p>
-                  <p><strong>Poster:</strong> <a href={movie.poster} target="_blank">Watch Poster</a></p>
-                  <p><strong>Trailer:</strong> <a href={movie.trailer} target="_blank">Watch Trailer</a></p>
-                  <p><strong>Run Time:</strong> {movie.runtime} minutes</p>
-                  <p><strong>Awards:</strong> {movie.awards}</p>
-                  <p><strong>Country:</strong> {movie.country}</p>
-                  <p><strong>Language:</strong> {movie.language}</p>
-                  <p><strong>Box Office:</strong> {movie.boxOffice}</p>
-                  <p><strong>Production:</strong> {movie.production}</p>
-                  <p><strong>Website:</strong> <a href={movie.website} target="_blank">Visit Website</a></p>
-                </div>
-              )}
-            </div>
-          ))}
-          {currentPageWatchedList > 0 && (
-            <button className='handleButtonList' onClick={handleShowLessWatchedList}>Show Less</button>
-          )}
-          {filteredWatchedList.length > (currentPageWatchedList + 1) * itemsPerPage && (
-            <button className='handleButtonList' onClick={handleShowMoreWatchedList}>Show More</button>
-          )}
+          {filteredWatchedList.slice(currentPageWatchedList * itemsPerPage, (currentPageWatchedList + 1) * itemsPerPage)
+            .map((movie) => (
+              <div key={movie.id}>
+                <h3>{movie.title}</h3>
+                <p><strong>Plot:</strong> {movie.plot}</p>
+                <button className='handleButton' onClick={() => removeFromWatched(movie)}>Remove from Watched</button>
+                <button className='toggleButton' onClick={() => toggleDetails(movie.id)}>
+                  {detailsVisible === movie.id ? 'Show Less' : 'Read More Info'}
+                </button>
+                {detailsVisible === movie.id && (
+                  <div>
+                    <p><strong>Year:</strong> {movie.year}</p>
+                    <p><strong>Genre:</strong> {movie.genre.join(', ')}</p>
+                    <p><strong>Rating:</strong> {movie.rating}</p>
+                    <p><strong>Director:</strong> {movie.director}</p>
+                    <p><strong>Actors:</strong> {movie.actors.join(', ')}</p>
+                    <p><strong>Poster:</strong> <a href={movie.poster} target="_blank">Watch Poster</a></p>
+                    <p><strong>Trailer:</strong> <a href={movie.trailer} target="_blank">Watch Trailer</a></p>
+                    <p><strong>Run Time:</strong> {movie.runtime} minutes</p>
+                    <p><strong>Awards:</strong> {movie.awards}</p>
+                    <p><strong>Country:</strong> {movie.country}</p>
+                    <p><strong>Language:</strong> {movie.language}</p>
+                    <p><strong>Box Office:</strong> {movie.boxOffice}</p>
+                    <p><strong>Production:</strong> {movie.production}</p>
+                    <p><strong>Website:</strong> <a href={movie.website} target="_blank">Visit Website</a></p>
+                  </div>
+                )}
+              </div>
+            ))}
+          <ReactPaginate
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={Math.ceil(filteredWatchedList.length / itemsPerPage)}
+            onPageChange={changePageWatchedList}
+            containerClassName={"pagination"}
+          />
         </div>
       </div>
     </div>
@@ -177,5 +172,3 @@ function MovieListManager() {
 }
 
 export default MovieListManager;
-
-
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type {AppProps} from"next/app"
 import "../pages/home/main.scss"
 import "../pages/MovieList/main.scss"
+
 function App({Component, pageProps}: AppProps){
     return <Component {...pageProps} />
 
